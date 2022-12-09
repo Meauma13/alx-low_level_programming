3a20e414e7caa02682f1adfee32b73c1924a5311
Anthony Aligbe <aumatony@gmail.com>
2022-12-09 05:33:41 -0800

    Test a random number for positivity or negativity

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+/*more headers goes there*/
+
+/*
+*
+* main - Entry point
+*
+* Return: 0,for successful execution
+*
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n > 0)
+	{
+		printf("%lu is positive\n", n);
+	}
+	else if (n = 0)
+	{
+		printf("%d  is zero\n", n);
+	}
+	else
+	{
+		printf("%l is negative\n", n);
+	}
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This project contains solutions to various C low-level programming tasks using conditional if-else and looping while statements.
