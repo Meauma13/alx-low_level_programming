777b5c5a7c43283f430f4d886ae010c48136cb34
Anthony Aligbe <aumatony@gmail.com>
2022-12-13 22:55:46 -0800

    Test for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,12 @@
 
 /**
 * _islower - test for lowercase character
-*
+* @c: expected parameter to be tested
 * Return: 1, if lowercase ,otherwise 0
 */
 
 /*_islower tests for lowercase character*/
-int _islower(int c) /*parameter int c expects integer*/
+int _islower(int c)
 {
 	if ((c > 96) && (c < 123))
 	{
