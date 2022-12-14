dca7fb589c5639816ae8eb22148955cc0399306f
Anthony Aligbe <aumatony@gmail.com>
2022-12-14 06:46:08 -0800

    Print the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _abs - prints the absolute value of a number
+* @n: expected parameter to be tested
+* Return: the absolute value of nnđ
+*
+*/
+
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n == 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n + n - n);
+	}
+}
