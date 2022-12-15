394fd28540482d7045f94709b6398281090ef8b1
Anthony Aligbe <aumatony@gmail.com>
2022-12-15 06:38:05 -0800

    Print n times table starting from 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,7 +15,7 @@ void print_times_table(int n)
 	if ((n > 15) || (n < 0))
 		exit(0);
 
-	int r, c, prod, d;
+	int r, c, prod;
 
 	r = 0;
 	while (r <= n)
@@ -27,10 +27,9 @@ void print_times_table(int n)
 			if (prod > 99)
 			{
 				_putchar(' ');
-				d = prod % 100;
 				_putchar((prod / 100) + '0');
-				_putchar((d / 10) + '0');
-				_putchar((d % 10) + '0');
+				_putchar(((prod % 100) / 10) + '0');
+				_putchar(((prod % 100) % 10) + '0');
 			}
 			else if (prod > 9)
 			{
