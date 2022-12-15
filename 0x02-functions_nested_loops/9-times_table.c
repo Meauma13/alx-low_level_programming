83cddff911285869cd04fb2b4bd00d07e5878733
Anthony Aligbe <aumatony@gmail.com>
2022-12-14 22:43:43 -0800

    Print the times table up to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,19 +24,20 @@ void times_table(void)
 				_putchar((prod % 10) + '0');
 				_putchar(',');
 			}
+			else if ((prod == 0) && (c
+ != 0))
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0
+');
+			}
+			else if (prod == 0)
+			{
+				_putchar(prod + '0');
+				_putchar(',');
+			}
 			else if ((prod < 10) && ((c != 0) && (r != 0)))
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(prod + '0');
-				_putchar(',');
-			}
-			else if ((prod == 0) && (r == 0))
-			{
-				_putchar(prod + '0');
-				_putchar(',');
-			}
-			else if ((prod == 0) && (c != 0))
 			{
 				_putchar(' ');
 				_putchar(' ');
