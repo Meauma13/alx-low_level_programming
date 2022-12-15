3d4d24ab1820807ac827f2025da9eab3e4e3d2f0
Anthony Aligbe <aumatony@gmail.com>
2022-12-15 12:33:08 -0800

    Print the first 9898nfi

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - prints the first 98 fibonacci numbers
+*
+* Return: 0, if successful
+*/
+
+int main(void)
+{
+	int n, m;
+	long a = 1;
+	long  b = 2;
+	long sum = 0;
+
+	for (n = 1; n <= 2; n++)
+	{
+		printf("%d, ", n);
+	}
+
+	for (m = 3; m <= 98; m++)
+	{
+		sum = a + b;
+		printf("%lu", sum);
+
+		if (m != 98)
+			printf(", ");
+		a = b;
+		b = sum;
+	}
+	printf("\n");
+	return (0);
+}
