1805d14895891f2fd119001cc8629af3d25521ec
Anthony Aligbe <aumatony@gmail.com>
2022-12-14 20:12:32 -0800

    Print the times table up to 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* times_table - prints the times table up to 9
+* Return: void, if successful
+*
+*/
+
+void times_table(void)
+{
+	int r, c, prod;
+
+	r = 0;
+	while (r < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			prod = r * c;
+			if (prod > 9)
+			{
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			else
+			{
+				_putchar(prod + '0');
+			}
+
+			if (c == 9)
+			{
+				_putchar('\n');
+				break;
+			}
+
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('\n');
+
+			c++;
+		}
+		r++;
+	}
+}
