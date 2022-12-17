a9cdf5e4d8021d1cd92eae57e80a0e8d6d32ee88
Anthony Aligbe <aumatony@gmail.com>
2022-12-17 02:42:25 -0800

    Multiply 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - multiplies 2 integers
+*
+* @c: expeceted parameter for testing
+*
+* Return: (1), if successful
+*/
+
+int _isdigit(int a, int b)
+{
+	return (a * b);
+}
