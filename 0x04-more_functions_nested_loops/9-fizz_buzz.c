47fce3b566337d6571dae9906727568a3b319262
Anthony Aligbe <aumatony@gmail.com>
2022-12-19 00:01:29 -0800

    Prints numbers 1 to 100 with special words for multiples of 3 and 5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
 * main - prints numbers 1 to 100 with special words for multiples of 3 and 5
@@ -14,58 +13,35 @@ int main(void)
 
 	for (n = 1; n < 101; n++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if (n == 100)
 		{
-			printFizz();
-			printBuzz();
-			_putchar(32);
+			printf("Buzz");
+		}
+		else if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz ");
 		}
 		else if (n % 3 == 0)
 		{
-			printFizz();
-			_putchar(32);
+			printf("Fizz ");
 		}
 		else if (n % 5 == 0)
 		{
-			printBuzz();
-			_putchar(32);
+			printf("Buzz ");
 		}
 		else
 		{
 			if (n < 10)
 			{
-				_putchar(n + '0');
+				printf("%d ", n);
 			}
 			else if (n > 10 && n < 100)
 			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
+				printf("%d", (n / 10));
+				printf("%d ", (n % 10));
 			}
-			_putchar(32);
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 	return (0);
-}
-
-/**
-* printFizz - prints the word Fizz
-*/
-void printFizz(void)
-{
-	_putchar('F');
-	_putchar('i');
-	_putchar('z');
-	_putchar('z');
-}
-
-/**
-* printBuzz - prints the word Buzz
-*/
-void printBuzz(void)
-{
-	_putchar('B');
-	_putchar('u');
-	_putchar('z');
-	_putchar('z');
 }
